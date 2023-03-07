ab4d711eb56ad6ce094cd857d22af782a8361fe5
myusuf963 <mohammad963yusuf@gmail.com>
2023-03-07 17:42:13 +0000

    clean up and rename vars

--- iwt-cw.js
+++ iwt-cw.js
@@ -1,11 +1,10 @@
 document.getElementById("year-op").value = "=";
-$(document).ready(function() {
-
+$(document).ready(function () {
   // Clear all form inputs
   $("#query-form :input").val("");
-  
+
   // Handle "Send Query" button click
-  $("#send-query").click(function() {
+  $("#send-query").click(function () {
     // Get form data
     var formData = {
       file: $("#file").val(),
@@ -13,11 +12,17 @@ $(document).ready(function() {
       yearOperator: $("#year-op").val(),
       tournament: $("#tournament").val(),
       winner: $("#winner").val(),
-      runnerUp: $("#runner-up").val()
+      runnerUp: $("#runner-up").val(),
     };
 
     // Check if no form input is entered
-    if ($.isEmptyObject(formData) || (formData.year == "" && formData.tournament == "" && formData.winner == "" && formData.runnerUp == "")) {
+    if (
+      $.isEmptyObject(formData) ||
+      (formData.year == "" &&
+        formData.tournament == "" &&
+        formData.winner == "" &&
+        formData.runnerUp == "")
+    ) {
       // Set formData to have an empty value for all form fields except for the "file" field
       formData = {
         file: $("#file").val(),
@@ -25,42 +30,54 @@ $(document).ready(function() {
         yearOperator: "",
         tournament: "",
         winner: "",
-        runnerUp: ""
+        runnerUp: "",
       };
     }
 
     // Send GET request to iwt-cw.php with form data as query string
-    $.get("iwt-cw.php", formData, function(data) {
-      // Clear existing table rows
-      $("#results-table tbody").empty();
-      // Add new table rows for each result
-      $.each(data, function(index, result) {
-        var cleanResult = {...result, runnerup: result['runner-up']}
-        $("#results-table tbody").append(
-          "<tr><td>" + cleanResult.tournament + "</td>" +
-          "<td>" + cleanResult.year + "</td>" +
-          "<td>" + cleanResult.winner + "</td>" +
-          "<td>" + cleanResult.runnerup + "</td></tr>"
-        );
-      });
-    }, "json").fail(function(xhr, status, error) {
+    $.get(
+      "iwt-cw.php",
+      formData,
+      function (data) {
+        // Clear existing table rows
+        $("#results-table tbody").empty();
+        // Add new table rows for each result
+        $.each(data, function (index, result) {
+          var cleanResult = { ...result, runnerup: result["runner-up"] };
+          $("#results-table tbody").append(
+            "<tr><td>" +
+              cleanResult.tournament +
+              "</td>" +
+              "<td>" +
+              cleanResult.year +
+              "</td>" +
+              "<td>" +
+              cleanResult.winner +
+              "</td>" +
+              "<td>" +
+              cleanResult.runnerup +
+              "</td></tr>"
+          );
+        });
+      },
+      "json"
+    ).fail(function (xhr, status, error) {
       // Show error message if there is an error
       alert("Error: " + xhr.responseText);
     });
   });
-  
+
   // Handle "Clear Input" button click
-  $("#clear-input").click(function() {
+  $("#clear-input").click(function () {
     // Clear all form inputs
     $("#query-form :input").val("");
   });
-  
+
   // Handle "Clear Output" button click
-  $("#clear-output").click(function() {
+  $("#clear-output").click(function () {
     // Clear all table rows
     $("#results-table tbody").empty();
   });
-  
 });
 
-console.log('vewr')
+console.log("vewr");
